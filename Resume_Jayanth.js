f381b11fe18255660ec513ff49201bfaa9431d48
Jayanth9710 <68297123+Jayanth9710@users.noreply.github.com>
2021-06-20 10:49:53 +0530

    Add files via upload

---
+++ Resume_Jayanth.js
@@ -0,0 +1,117 @@
+resume={
+    "basic details": {
+      "name": "Jayanth Kumar",
+      "email": "kumarjayanth55@gmail.com",
+      "phone": "86670xxxxx",
+      "location": {
+        "address": "no.4,sandhukadai street,Big Bazzar",
+        "City": "Tiruchirapalli",
+        "State": "Tamil Nadu",
+        "Pincode": "620008"
+      },
+      
+    },
+    "Education Details": {
+      "Class X": [
+        {
+          "School": "Kendriya Vidyalaya No.1,Trichy",
+          "CGPA": "8.0",
+          
+        }
+      ],
+      "Class XII": [
+        {
+          "School": "Kendriya Vidyalaya No.1,Trichy",
+          "Percentage": "66%",
+          
+        }
+      ],
+      "Under Graduation": [
+        {
+          "College Name": "C.A.R.E School Of Engineering",
+          "University": "Affiliated to Anna University",
+          "Department": "Civil Engineering",
+          "CGPA": "6.45",
+          
+        }
+      ],
+      
+    },
+    "Work Experience": {
+      "CYIENT LTD": [
+        {
+          "Position": "GIS PLANNER",
+          "Location": "Noida,Delhi",
+          "Duration": "11/2020-03-2021",
+          "Achievements/Tasks": [
+            {
+              "1": "Creating Vecotr datasets ( Road Networks and its adjacent features) for many functionalities i.e. Inventory using ArcGIS and QGIS.",
+              "2": "Handling vector datasets efficiently in both ArcGis and QGIS for Processing",
+              
+            }
+          ],
+          
+        }
+      ],
+      "VizExperts": [
+        {
+          "Position": "GIS Executive",
+          "Location": "Delhi",
+          "Duration": "11/2019-11/2020",
+          "Achievements/Tasks": [
+            {
+              "1": "Digitization , ArcGIS , QGIS , AutoCAD and project delivery.",
+              "2": "Acknowledged for achieving targets and for consistency.",
+              "3": "Analysing satellite images, mosaicking rasters, using various raster and vector toolset in both ArcMap and QGIS."
+            }
+          ],
+          
+        }
+      ],
+      "Internship": [
+        {
+          "Company/Organization": "Public Works Department",
+          "Task": "Site supervision for construction of Chief Engineer Building at Trichy.",
+          "Duration": "12/2017-12/2017",
+          
+        }
+      ],
+      
+    },
+    "Skills": [
+      {
+        "1": "AutoCAD",
+        "2": "Arcmap",
+        "3": "Qgis",
+        "4": "Openlayers",
+        "5": "Geoserver",
+        
+      }
+    ],
+    "Personal Projects": [
+      {
+        "Project Title": "GIS Based integrated utility management system.",
+        "Problem Statement": "Problem statement for Smart India Hackathon 2018 Our proposed  solution is to develop a Web GIS based Integrated Utility  Management system for Utility Management. The system will enable access to spatial data by all through GUI Interface.",
+        "Duration": "03/2018-03/2018",
+        
+      }
+    ],
+    "Languages": [
+      {
+        "1": "English",
+        "2": "Tamil",
+        "3": "Marathi",
+        "4": "Hindi",
+        
+      }
+    ],
+    "Personal Interests": [
+      {
+        "1": "Music",
+        "2": "Maps",
+        "3": "Football",
+        "4": "Technology/Gadgets",
+        
+      }
+    ]
+  }
